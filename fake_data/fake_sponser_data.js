0b50dd471e0c53553187a79d8401c2be14a4dd2d
ahsieh53632 <alex53632@outlook.com>
2021-04-08 18:37:45 -0400

    init fake sponser data

---
+++ fake_data/fake_sponser_data.js
@@ -0,0 +1,23 @@
+export default fake_club_data = ([
+    {
+        name: "",
+        description: "",
+        apply_info: "",
+    },
+    {
+        name: "",
+        description: "",
+        apply_info: "",
+    },
+    {
+        name: "",
+        description: "",
+        apply_info: "",
+    },
+    {
+        name: "",
+        description: "",
+        apply_info: "",
+    },
+])
+
