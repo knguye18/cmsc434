498d6048bbbae86c289338c62b6d3b0a3c2d4dba
ahsieh53632 <alex53632@outlook.com>
2021-04-09 05:25:43 -0400

    clubs page and sponsors page
    	new file:   Clubs/filtering.js
    	new file:   Clubs/index.html
    	new file:   Clubs/style.css
    	new file:   Sponsor/filtering.js
    	modified:   Sponsor/index.html
    	modified:   Sponsor/style.css
    	new file:   components/keyboard/keyboard.css
    	new file:   components/keyboard/keyboard.js
    	renamed:    popup.css -> components/modal/popup.css
    	modified:   components/search_bar.css
    	modified:   dashboard.css
    	deleted:    fake_data/fake_calendar_info.js
    	deleted:    fake_data/fake_club_data.js
    	deleted:    fake_data/fake_sponser_data.js
    	new file:   imgs/clubs_icons/technica.jpg
    	new file:   imgs/sponsor_icons/google.jpg
    	modified:   index.html

---
+++ Sponsor/filtering.js
@@ -0,0 +1,100 @@
+const fake_sponsor_data = [
+    {
+        name: "Google",
+        description: "The Google O yerrr",
+        job_openings: "Sofware Engineer, Backend Enginner new Grad",
+        icon: "google.jpg",
+        events: ["4/10 4pm - 6pm o yerr", "4/20 5pm - 6pm group coding sess"],
+    },
+    {
+        name: "UMD Robotics",
+        description: "Nerds",
+        job_openings: "you have to be blind (worse that +5.0 both eyes) to prove your nerdyness",
+        icon: "technica.jpg",
+        events: [],
+    },
+    {
+        name: "Club1",
+        description: "Description1",
+        job_openings: "just apply mannn",
+        events: [],
+    },
+    {
+        name: "Club",
+        description: "A YER",
+        job_openings: "noooo shot",
+        events: [],
+    },
+];
+
+
+function onSearchBarChange(e) {
+    const query = e.target.value.toLowerCase();
+    const search_results = fake_sponsor_data.filter(x => x.name.toLowerCase().includes(query))
+    let formedHtml = ""
+    search_results.forEach((element, index) => {
+        const icon_src = "/imgs/sponsor_icons/" + element.icon;
+        const card = `
+            <button class="card" onclick="onClickCard(event, ${index});">
+                <img class="icon" src=${icon_src} />
+                <h5>${element.name}</h5>
+            </button>
+        `;
+
+        formedHtml += card;
+    })
+
+    document.getElementById("clubs-container").innerHTML = formedHtml;
+}
+
+function onClickCard(e, key) {
+    const obj = fake_sponsor_data[key]
+
+    document.getElementById("club-name").innerHTML = obj.name;
+    document.getElementById("club-description").innerHTML = obj.description;
+    document.getElementById("club-join").innerHTML = obj.job_openings;
+
+    let upcoming_events = "";
+
+    obj.events.forEach((element) => {
+        upcoming_events += `
+            <li>${element}</li>
+        `
+    })
+    
+    document.getElementById("modal").classList.toggle("hidden");
+    document.getElementById("events").innerHTML = upcoming_events;
+}
+
+const hideModal = (e) => {
+    document.getElementById("modal").classList.toggle("hidden");
+}
+
+function hideKeyboard() {
+    console.log('fff')
+    document.querySelectorAll(".keyboard").forEach(e => {
+        console.log(e)
+        e.classList.add("keyboard--hidden");
+    })
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    let formedHtml = "";
+    fake_sponsor_data.forEach((element, index) => {
+        const icon_src = "/imgs/sponsor_icons/" + element.icon;
+        const card = `
+            <button class="card" onclick="onClickCard(event, ${index});">
+                <img class="icon" src=${icon_src} />
+                <h5>${element.name}</h5>
+            </button>
+        `;
+
+        formedHtml += card;
+    })
+
+    document.getElementById("clubs-container").innerHTML = formedHtml;
+
+    document.getElementById("search-bar").addEventListener("input", onSearchBarChange);
+});
+
